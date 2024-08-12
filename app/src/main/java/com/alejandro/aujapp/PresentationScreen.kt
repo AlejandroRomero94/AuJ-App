0130cffcdc7bb3fbf1e10c61c085a1460f45814f
AlejandroRP <romeroporras94@gmail.com>
2024-08-12 10:20:55 +0200

    PresentationScreen reedited. RecorridoScreen texts done. Strings.xml updated

--- app/src/main/java/com/alejandro/aujapp/PresentationScreen.kt
+++ app/src/main/java/com/alejandro/aujapp/PresentationScreen.kt
@@ -36,7 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-@Preview
+
 @Composable
 fun PresentationScreen(){
 
