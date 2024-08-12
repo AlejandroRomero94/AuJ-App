f07985a87880a961c64410aba09396d3b0d64ddc
AlejandroRP <romeroporras94@gmail.com>
2024-08-12 10:02:36 +0200

    PresentationScreen Text done

--- app/src/main/java/com/alejandro/aujapp/PresentationScreen.kt
+++ app/src/main/java/com/alejandro/aujapp/PresentationScreen.kt
@@ -1,14 +1,42 @@
 package com.alejandro.aujapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+@Preview
 @Composable
 fun PresentationScreen(){
 
@@ -17,10 +45,67 @@ fun PresentationScreen(){
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = "Pantalla 1",
-                    fontSize = MaterialTheme.typography.titleMedium.fontSize,
-                    fontWeight = FontWeight.Medium)
+                Image(
+                    painter = painterResource(id = R.drawable.background_drawer),contentDescription = "background",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+                LazyColumn(modifier= Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = 120.dp,
+                        start = 24.dp,
+                        end = 24.dp,
+                        bottom = 40.dp
+                    ),
+horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                    item{
+                        Box(modifier = Modifier
+                            .size(200.dp)
+                            .size(200.dp),
+                            contentAlignment = Alignment.Center
+                        ){
+                            Image(painter = painterResource(id = R.drawable.auj_logo),
+                                contentDescription = "AuJ logo",
+                                modifier= Modifier
+                                    .fillMaxSize()
+                                    .clip(CircleShape)
+                                    .border(5.dp, Color.White, CircleShape) )
+
+                        }
+
+                        Text(
+                            text = stringResource(id = R.string.Presentation1),
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            color = Color.White,
+                            modifier = Modifier.padding(top=64.dp),
+                        )
+
+                        Text(
+                            text = stringResource(id = R.string.Presentation2),
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            color = Color.White,
+                            modifier = Modifier.padding(top=32.dp),
+                        )
+
+                        Text(
+                            text = stringResource(id = R.string.Presentation3),
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            color = Color.White,
+                            modifier = Modifier.padding(top=32.dp),
+                        )
+                    }
+                    
+                }
+                
+                
 
 }
 }
